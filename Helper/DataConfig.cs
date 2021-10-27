0b758110d8e4e7b53bd4ec166c6149f86babec73
PhucDVb <dovietphuc0131@gmail.com>
2021-10-27 09:10:48 +0700

    PhucDV: push source

---
+++ Helper/DataConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VB2QLNS.Helper
+{
+    class DataConfig
+    {
+        public static string ServerName = "DESKTOP-1BI8MPQ";
+        public static string ConnectionString = @"Data Source=" + ServerName + @";Initial Catalog=QLNS;Integrated Security=True";
+    }
+}
